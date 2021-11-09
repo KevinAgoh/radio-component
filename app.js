d444fa064ef8b3531cfac4e4ed5093ef65a8bc87
Kevin Agoh <kevinagoh@hotmail.fr>
2021-11-09 17:35:37 +0100

    implemented a toogle function for the image

--- app.js
+++ app.js
@@ -1,13 +1,30 @@
-const stations = document.querySelector('.radio');
+const stations = document.querySelector('.stations');
+const radioInfos = document.querySelectorAll('.radio-text');
 
 fetch('https://teclead.de/recruiting/radios')
-  .then(response => response.json())
-  .then((data) => {
-    data.radios.forEach((result) => {
-      const radio = `<div class='radio-text'>
-        <p>${result.name} ${result.frequency}</p>
-      </div>`;
-      stations.insertAdjacentHTML('beforeend', radio)
-    })
-    console.log(data.radios);
-  });
+.then(response => response.json())
+.then((data) => {
+  console.log(data.radios);
+  data.radios.forEach((result) => {
+    const radioStation = `<div id='radio-text'>
+      <button id='btn-radio'>${result.name}</button> <p>${result.frequency}</p>
+    </div>
+    <div class="radio-image hidden">
+      <i class="fas fa-minus-circle"></i>
+      <img src="${result.image}" alt="radio station image" />
+      <i class="fas fa-plus-circle"></i>
+    </div>`;
+    stations.insertAdjacentHTML('beforeend', radioStation);
+  })
+});
+
+// example of javascript event delegation
+
+stations.addEventListener('click', function(e) {
+  const images = document.querySelectorAll('.radio-image');
+
+  if (e.target && e.target.id == 'btn-radio') {
+    images.classList.toggle('hidden');
+  }
+  
+});
